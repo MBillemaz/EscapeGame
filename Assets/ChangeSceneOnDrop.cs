7a5e183177ef1da1a9aac055ed3c30cbf53339fa
Maxime Billemaz <maxime.bill@hotmail.fr>
2018-11-28 15:30:42 +0100

    Change scene on drop sword on the altar

---
+++ Assets/ChangeSceneOnDrop.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnDrop : MonoBehaviour
+{
+
+    public VRTK_SnapDropZone dropZone;
+    // Use this for initialization
+    void Start()
+    {
+        dropZone.ObjectSnappedToDropZone += CheckItem;
+    }
+
+    protected virtual void CheckItem(object sender, SnapDropZoneEventArgs e)
+    {
+        GameObject item = dropZone.GetCurrentSnappedObject();
+        if (item.name == "Epee")
+        {
+            StartCoroutine("ChangeScene", "Game");
+            
+        }
+    }
+
+    IEnumerator ChangeScene(string scene)
+    {
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+
+        yield return null;
+    }
+}
+
