e582fabb5dabd6e1f37ab6b1dc4df8ebe6235f66
BaptDu <baptiste.dumont@ynov.com>
2018-12-20 16:19:20 +0100

    update RadialMenu Script

---
+++ Assets/RadialConfigScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RadialConfigScript : MonoBehaviour
+{
+    // Use this for initialization
+
+    // Update is called once per frame
+    public void SwitchScene(string name)
+    {
+        if (name == "Exit")
+        {
+            StartCoroutine("ChangeScene", "Menu");
+        }
+        else if (name == "Save")
+        {
+            Debug.Log("SAVE");
+        }
+        else if (name == "Help")
+        {
+            Debug.Log("HELP");
+        }
+    }
+
+    IEnumerator ChangeScene(string scene)
+    {
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+
+        yield return null;
+    }
+}
