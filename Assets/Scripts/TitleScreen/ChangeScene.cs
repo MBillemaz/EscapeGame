59626b9838add2e347e99a3ad81406a3478fa69c
Maxime Billemaz <maxime.bill@hotmail.fr>
2018-12-21 10:17:17 +0100

    Add title scene + create script folder

---
+++ Assets/Scripts/TitleScreen/ChangeScene.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK;
+
+public class ChangeScene : MonoBehaviour {
+
+    public int secToWait;
+    public string sceneName;
+    private IEnumerator coroutine;
+	// Use this for initialization
+	void Start () {
+        coroutine = WaitForScene();
+        StartCoroutine(coroutine);
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var controller = VRTK_DeviceFinder.GetControllerRightHand().GetComponent<VRTK.VRTK_ControllerEvents>();
+
+        if (controller.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.TriggerPress)){
+            StopCoroutine(coroutine);
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+	}
+
+    IEnumerator WaitForScene()
+    {
+        yield return new WaitForSeconds(secToWait);
+
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/TitleScreen/frontOfCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+public class frontOfCamera : MonoBehaviour {
+
+    // La position de l'utilisateur est null lors du start. On stocke donc la valeur et dés on lance le traitement dés qu'elle n'est pas null
+    Transform userPos = null;
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.userPos == null)
+        {
+            this.userPos = VRTK_DeviceFinder.HeadsetTransform();
+            this.transform.position = this.userPos.position + new Vector3(0, 0, 10);
+        }
+    }
+}
