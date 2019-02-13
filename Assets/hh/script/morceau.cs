c7a38a038c19b05c148a7fd57aca3c0d7be444e7
HONGOIS Hubert <hubert.hongois@gmail.com>
2019-02-13 10:59:17 +0100

    hubert branche

---
+++ Assets/hh/script/morceau.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class morceau : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject fils;
+    public static int n = 0;
+	void Start () {
+        n++;
+        fils.GetComponent<Rigidbody>().AddExplosionForce(2, fils.transform.position, 2, 2);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //fils.transform.parent.SetParent(GameObject.FindGameObjectWithTag("Bloc").transform);
+	}
+}
