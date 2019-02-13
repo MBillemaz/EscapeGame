0f6e333f5b745f179c0cd3ede9c950a0ad8c1b93
Steven Ly <soranyuxe@gmail.com>
2019-02-13 11:33:00 +0100

    La balance
    Prefab de la balance et des poids, des indices avec leurs scripts.

---
+++ Assets/Prefabs/Prefabs_balance/Script/Miseazero.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Miseazero : MonoBehaviour
+{
+
+    public Quaternion originalRotationValue; // declare this as a Quaternion
+    float rotationResetSpeed = 1.0f;
+
+    /*public Rigidbody r_Rigidbody;*/
+    public float max = 133;
+    public float min = 47;
+
+    // Use this for initialization
+    void Start()
+    {
+        originalRotationValue = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* Vector3 currentRotation = transform.localRotation.eulerAngles;
+         if (currentRotation.x >= max)
+            {
+                r_Rigidbody.rotation = Quaternion.Euler(90f,0f,0f);
+            }
+
+        }*/
+        //rotate selected piece
+        Vector3 currentRotation = transform.localRotation.eulerAngles;
+        if (currentRotation.x > max || currentRotation.x<min)
+            transform.rotation = Quaternion.Slerp(transform.rotation, originalRotationValue, Time.time * rotationResetSpeed);
+    }
+}
