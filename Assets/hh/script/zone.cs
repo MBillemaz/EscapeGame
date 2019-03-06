4649a4b82ed4507b3ad6f4a2e388a4c8721be063
HONGOIS Hubert <hubert.hongois@gmail.com>
2019-03-06 15:54:44 +0100

    test 3 non stable

--- Assets/hh/script/zone.cs
+++ Assets/hh/script/zone.cs
@@ -6,17 +6,37 @@ using System.Collections;
 public class zone : MonoBehaviour {
 
     public GameObject[] tmpObjets;
+    public GameObject autel;
     public GameObject[] listMorceaux;
     public int i = 0;
 
+    private void Awake()
+    {
+        
+    }
     void Start()
     {
         //Instantier l'objet entier a reconstituer
         listMorceaux = new GameObject[GameObject.FindGameObjectsWithTag("Morceaux").Length];
         listMorceaux = GameObject.FindGameObjectsWithTag("Morceaux");
         tmpObjets = new GameObject[GameObject.FindGameObjectsWithTag("Morceaux").Length];
+        autel = GameObject.FindGameObjectWithTag("autel");
     }
+    private void allMorceaux()
+    {
+        if (i == tmpObjets.Length)
+        {
+            Debug.Log("On va détruire l'objet");
+            //Appel de l'animation quand tous les objets sont présents
 
+            //Destruction de tous les morceaux
+            for (var y = 0; y < i; y++)
+            {
+                Destroy(tmpObjets[y]);
+            }
+
+        }
+    }
     // Détecte si un morceaux a été trouvé, si tous les morceaux sont trouvés fait appel a l'animation pour reconstituer l'objet
     private void OnTriggerEnter(Collider other)
     {
@@ -30,56 +50,45 @@ public class zone : MonoBehaviour {
         {
             if (listMorceaux[z].name.Equals(other.name))
             {
+                Debug.Log(listMorceaux[z].transform.position);
                 tmp = listMorceaux[z];
-                a = true;
-            }
-
-        }
-        if (a)
-        {
-            for (int y = 0; y < tmpObjets.Length; y++)
-            {
-
-                if(tmpObjets[y] != null)
-                {
-                    if (tmpObjets[y].Equals(tmp))
+  
+                    for (int y = 0; y < tmpObjets.Length; y++)
                     {
-                        tmp2 = 1;
+
+                        if (tmpObjets[y] != null)
+                        {
+                            if (tmpObjets[y].Equals(tmp))
+                            {
+                                tmp2 = 1;
+                            }
+                        }
+
+                    }
+                    // si l'objet n'est pas présent on l'ajoute et on incremente i
+                    if (tmp2 == 0)
+                    {
+                        tmpObjets[i] = tmp;
+                        tmpObjets[i].transform.position = autel.transform.position + listMorceaux[z].transform.position;
+                    Debug.Log(tmpObjets[i].transform.position);
+                        i++;
+                        Debug.Log(i);
+                        Debug.Log(tmp);
                     }
                 }
-                
-            }
-            // si l'objet n'est pas présent on l'ajoute et on incremente i
-            if (tmp2 == 0)
-            {
-                tmpObjets[i] = tmp;
-                i++;
-                Debug.Log(i);
-                Debug.Log(tmp);
-            }
             allMorceaux();
+
         }
+
+    }
+        
         //morceaux.get
         //GameObject tmp = GameObject.FindGameObjectWithTag(other.name);
         // On regarde si l'objet n'est pas deja dans la liste des morceaux formant l'objet principale
         
     }
 
-    private void allMorceaux()
-    {
-        if(i == tmpObjets.Length)
-        {
-            Debug.Log("On va détruire l'objet");
-            //Appel de l'animation quand tous les objets sont présents
-
-            //Destruction de tous les morceaux
-            for (var y = 0; y < i; y++)
-            {
-                Destroy(tmpObjets[y]);
-            }
-
-        }
-    }
+   
     
 
-}
+
