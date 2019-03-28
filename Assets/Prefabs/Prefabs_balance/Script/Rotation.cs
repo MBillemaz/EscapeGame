c40aa7049685c04a5e507b672986d993138a845a
Maxime Billemaz <maxime.bill@hotmail.fr>
2019-03-28 15:26:23 +0100

    Try to fix rotation

--- Assets/Prefabs/Prefabs_balance/Script/Rotation.cs
+++ Assets/Prefabs/Prefabs_balance/Script/Rotation.cs
@@ -6,25 +6,24 @@ using UnityEngine;
 public class Rotation : MonoBehaviour
 {
     public Rigidbody m_Rigidbody;
-    public float max, min;
+    public float min, max;
     public Quaternion originalRotationValue;
     float rotationResetSpeed = 0.5f;
+    private Rigidbody body;
     // Use this for initialization
 
     void Start()
     {
         originalRotationValue = transform.rotation;
+        this.body = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
 
-    }
-    public void FixedUpdate()
-    {
-
-        Vector3 currentRotation = transform.localRotation.eulerAngles;
+        Quaternion currentRotation = transform.rotation;
+        float x = currentRotation.x;
         /*currentRotation.x = Mathf.Clamp(currentRotation.x, minRotation, maxRotation);
          transform.localRotation = Quaternion.Euler(currentRotation);
 
@@ -46,9 +45,27 @@ public class Rotation : MonoBehaviour
             m_Rigidbody.constraints = RigidbodyConstraints.None;
         }
         */
-        if (currentRotation.x > max || currentRotation.x < min)
+        Debug.Log(body.angularVelocity.x);
+        //Debug.Log("before " + min + " " + max + " "+  x + " " + currentRotation.y + " " + currentRotation.z);
+        if (x > max && body.angularVelocity.x > 0)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, originalRotationValue, rotationResetSpeed * Time.time);
+            // transform.rotation = Quaternion.Euler(max, 0, 0);
+            // transform.localRotation.eulerAngles = 3;
+            //body.isKinematic = true;
+            //body.angularVelocity = -body.angularVelocity*3;
+            body.angularVelocity = Vector3.zero;
+            // Debug.Log("after max " + transform.rotation.x + " " + transform.rotation.y + " " + transform.rotation.z);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, originalRotationValue, rotationResetSpeed * Time.time);
+        }
+        else if (x < min && body.angularVelocity.x < 0)
+        {
+            // transform.rotation = Quaternion.Euler(min, 0, 0);
+            //body.isKinematic = true;
+            // Debug.Log("after min " + transform.rotation.x + " " + transform.rotation.y + " " + transform.rotation.z);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, originalRotationValue, rotationResetSpeed * Time.time);
+        } else
+        {
+            //body.isKinematic = false;
         }
 
     }
