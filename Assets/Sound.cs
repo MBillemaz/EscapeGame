1cd7d6985a6fbf8690a8a037397a96c14766be86
Gianni <ganni.scantamburlo@ynov.com>
2019-11-07 12:22:25 +0100

    Sound effects
    Ajout de bruit de pas (pas testé)
    Ajout ambiance sonore à la scène Title
    Ajout de bruitages différents lorsque le casque de la scène Menu tombe au sol

---
+++ Assets/Sound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour {
+
+    public AudioSource metalDrop1;
+    public AudioSource metalDrop2;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void playmetalDrop1()
+    {
+        metalDrop1.Play();
+    }
+
+    public void playmetalDrop2()
+    {
+        metalDrop2.Play();
+    }
+
+    void onTriggerEnter()
+    {
+        metalDrop1.Play();
+    }
+}
